034a595047656ccc686eb77ea10eaf8bb73209ab
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-19 17:39:48 +0530

    heroku

--- pyconfig.py
+++ pyconfig.py
@@ -2,7 +2,7 @@ import os
 
 API_HASH = os.getenv("API_HASH")
 API_ID = int(os.getenv("API_ID"))
-HEROKU_API = os.getenv("HEROKU_API")
+HEROKU_API = os.getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 PY_SESSION = os.getenv("SESSION")
 PREFIX = os.environ.get("PREFIX")
