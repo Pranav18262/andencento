8fe54ed11b80f354f7162256392fe6f6530455f6
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-19 16:52:42 +0530

    pyconfig

---
+++ pyconfig.py
@@ -0,0 +1,11 @@
+import os
+
+API_HASH = os.getenv("API_HASH")
+API_ID = int(os.getenv("API_ID"))
+HEROKU_API = os.getenv("HEROKU_API")
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+PY_SESSION = os.getenv("SESSION")
+PREFIX = os.environ.get("PREFIX")
+LOG_CHAT = int(os.getenv("LOG_CHAT"))
+MONGO_URI = os.getenv("MONGO_URI")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
