0f509f4eb5f0fc48ac9a438f816709ed1fd5264d
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-19 16:51:34 +0530

    client import

--- userbot/pyrogram/__init__.py
+++ userbot/pyrogram/__init__.py
@@ -1,11 +1,8 @@
 import logging
 import sys
 import time
-import motor.motor_asyncio
-import pyromod.listen
-from pyrogram import Client, errors
-from config import API_HASH, API_ID, PY_SESSION, BOT_TOKEN
 import logging
+from .client import *
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -17,14 +14,3 @@ LOGGER = logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 HELP = {}
 CMD_HELP = {}
-
-StartTime = time.time()
-
-API_ID = API_ID
-API_HASH = API_HASH
-PY_SESSION = PY_SESSION
-BOT_TOKEN = BOT_TOKEN
-
-
-iamnoob = Client(PY_SESSION, api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-    
