6bad9c48469fbd1c772316404a6aaa65023f5c75
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-19 12:14:53 +0530

    vc

---
+++ userbot/plugins/vc.py
@@ -0,0 +1,19 @@
+from userbot import *
+
+@bot.on(admin_cmd(pattern="playvc$",
+))
+async def _(e):
+    zz = await eor(e, "`VC bot started...`")
+    er, out = await bash("python userbot/bot.py")
+    LOGS.info(er)
+    LOGS.info(out)
+    if er:
+        msg = f"Failed {er}\n\n{out}"
+        if len(msg) > 4096:
+            with open("vc-error.txt", "w") as f:
+                f.write(msg.replace("`", ""))
+            await e.reply(file="vc-error.txt")
+            await zz.delete()
+            remove("vc-error.txt")
+            return
+        await zz.edit(msg)
