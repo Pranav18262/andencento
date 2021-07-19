26d84b2eee1d67f654c076c19f29476b3fdb4754
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-19 15:29:56 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,9 +5,9 @@ from distutils.util import strtobool as sb
 os.system("pip install -r r.txt")
 # Temprorary Purpose Soon will be remoced
 import subprocess
-import bashCommand
+import bashCmd
 print ("start")
-bashCommand("start.sh")
+bashCmd("start.sh")
 print ("end")
 op = os.environ.get("REQ", None)
 if {op} == "False" or None:
