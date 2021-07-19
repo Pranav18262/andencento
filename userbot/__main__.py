2171755ab14c831b8e06059e68c0a6c42854a7b8
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-19 12:35:51 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -11,7 +11,7 @@ from config import Config
 from . import *
 from .utils import *
 from .session.main import *
-os.system("python3 -m vcbot/bot")
+os.system("python3 -m bot")
 
     
 # let's get the bot ready                    
