df5baf5da744412325fc07ed022af3a81e1f7d0f
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-19 12:21:52 +0530

    Update vc.py

--- userbot/plugins/vc.py
+++ userbot/plugins/vc.py
@@ -4,7 +4,7 @@ from userbot import *
 ))
 async def _(e):
     zz = await eor(e, "`VC bot started...`")
-    er, out = await bash("python vcbot/bot.py")
+    er, out = await pythn("python -m vcbot/bot.py")
     LOGS.info(er)
     LOGS.info(out)
     if er:
