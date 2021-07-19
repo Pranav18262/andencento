23ae110375a091c80f544d65a175fe3ba332f267
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-19 18:23:39 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -4,8 +4,6 @@ import time
 op = os.environ.get("PYRO", None)
 if {op} == "True" or "TRUE":
     os.system("pip install -r pyro.txt")
-    os.system("pip uninstall tgCrypto")
-    os.system("pip uninstall telethon")
     from userbot.pyrogram import *
     app = client
 else:
