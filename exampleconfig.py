0de0c5150af78ce8884b129503dfff761033789c
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-26 17:56:09 +0530

    KK

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,7 +1,5 @@
 from userbot.config import Config
-
-
-class Development(Config):
+class Config(Config):
     # get this values from the my.telegram.org
     APP_ID = 6
     API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
